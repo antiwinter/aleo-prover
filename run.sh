648487be8ddf773220952fe9024e03bb0891a58a
antiwinter <antiwinter@126.com>
2022-07-08 11:27:54 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #cargo run --release -- -a aleo1cev9um90a3cvlxx7zzfxxz7v28drvyugzf7g77c8tpppj9s5uvps75lumg -p  pool.hamp.app:4200 -t 1 --debug
-cargo run --release -- -a aleo1cev9um90a3cvlxx7zzfxxz7v28drvyugzf7g77c8tpppj9s5uvps75lumg -p  172.16.1.101:8888 -t 1 --debug
+cargo run --release -- -a aleo1cev9um90a3cvlxx7zzfxxz7v28drvyugzf7g77c8tpppj9s5uvps75lumg -p  127.0.0.1:8888 -t 1 --debug
